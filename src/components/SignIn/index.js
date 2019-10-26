4481e39361dcc898851add7c1c4a4aaccd2ed7f4
Zayan S. Khan <zayan_k@icloud.com>
2019-10-26 21:47:54 +0100

    Initial commit

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class SignIn extends React.Component {
+  render() {
+    return <h1>test</h1>
+  }
+}
+export default SignIn
---
+++ src/components/TC/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class TC extends React.Component {
+  render() {
+    return <h1>death</h1>
+  }
+}
+export default TC
