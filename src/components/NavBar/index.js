457ee11dfb163db04644565ffe1fac4ed74c9e5d
zayansalman <zayan_k@icloud.com>
2019-10-27 01:23:01 +0100

    made form for input only visual doesnt do anything lol

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,21 +4,16 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form"; 
 
 // import "./NavBar.css";
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+const ExampleToast = () => {
+//   const [show, toggleShow] = useState(true);
 
   return (
     <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
+     
     </>
   );
 };
@@ -26,13 +21,33 @@ const ExampleToast = ({ children }) => {
 const App = () => (
   <Container className="p-3">
     <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast className="toast">
-        We now have Toasts
-        <span role="img" aria-label="tada">
+      <h1 className="header">Enter your locations</h1>
+
+      {/* <ExampleToast className="toast"> */}
+        {/* We now have Toasts */}
+        {/* <span role="img" aria-label="tada">
           🎉
-        </span>
-      </ExampleToast>
+        </span> */}
+      {/* </ExampleToast> */}
+
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label></Form.Label>
+                <Form.Control type="email" placeholder="Type a postcode here" />
+                <Form.Text className="text-muted">
+                 Enter a location to be commuted from.
+                 </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label></Form.Label>
+                <Form.Control type="email" placeholder="Type another postcode here" />
+                <Form.Text className="text-muted">
+                 Enter another location to be commuted from.
+                 </Form.Text>
+            </Form.Group>
+        </Form>
+
     </Jumbotron>
   </Container>
 );
