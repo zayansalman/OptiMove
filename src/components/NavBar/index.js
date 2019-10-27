3fd8dcff36927e69929eeeddd52bf8c6bedeb151
zayansalman <zayan_k@icloud.com>
2019-10-27 02:09:59 +0000

    working input and render input

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,55 +1,80 @@
-import React, { useState } from "react";
+import React, {createRef, createClass} from "react";
+import ReactDOM from "react-dom"; 
 
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form"; 
 
-// import "./NavBar.css";
-
-const ExampleToast = () => {
-//   const [show, toggleShow] = useState(true);
-
-  return (
-    <>
-     
-    </>
-  );
-};
-
-const App = () => (
+class MyNav extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { postcode: ''}; 
+        }
+        handleChange = event => {
+            this.setState({postcode: event.target.value}); 
+        };
+    render(){
+  return ( 
+    <React.Fragment>
   <Container className="p-3">
     <Jumbotron>
       <h1 className="header">Enter your locations</h1>
-
-      {/* <ExampleToast className="toast"> */}
-        {/* We now have Toasts */}
-        {/* <span role="img" aria-label="tada">
-          🎉
-        </span> */}
-      {/* </ExampleToast> */}
-
         <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label></Form.Label>
-                <Form.Control type="email" placeholder="Type a postcode here" />
-                <Form.Text className="text-muted">
-                 Enter a location to be commuted from.
-                 </Form.Text>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+             <Form.Control 
+             type="textarea" 
+             placeholder="Type a postcode here" 
+             componentClass="input"
+             value={this.state.postcode}
+             onChange={this.handleChange}
+            />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Control type="textarea" placeholder="Type another postcode here" />  
             </Form.Group>
 
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label></Form.Label>
-                <Form.Control type="email" placeholder="Type another postcode here" />
-                <Form.Text className="text-muted">
-                 Enter another location to be commuted from.
-                 </Form.Text>
-            </Form.Group>
-        </Form>
-
+            <Button variant="primary" type="submit">
+    Submit
+  </Button>
+    </Form>
+    <p>entered data test: {this.state.postcode}</p>
     </Jumbotron>
+    
   </Container>
-);
+ </React.Fragment>
+  )
+    }
+}
 
-export default App;
+// class MyNav extends React.Component {
+
+//     constructor(props){
+//       super(props);
+//       this.state = { username: '' };
+//     }
+   
+//     handleChange = event => {
+//       this.setState({ username: event.target.value });
+//     };
+   
+//     render() {
+//       return (
+//         <React.Fragment>
+//           <form>
+//             <label htmlFor="username">username</label>
+//             <input
+//               type="text"
+//               name="username"
+//               value={this.state.username}
+//               onChange={this.handleChange}
+//             />
+//           </form>
+   
+//           <h3>Your username is: {this.state.username}</h3>
+//         </React.Fragment>
+//       );
+//     }
+//    }
+
+export default MyNav; 
